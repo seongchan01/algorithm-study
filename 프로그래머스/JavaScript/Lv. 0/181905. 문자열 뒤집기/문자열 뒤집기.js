54f42fdb78843a27d7d193a14b787533d2631abf
Seongchan Park <goslow0628@gmail.com>
2026-04-05 23:53:07 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/JavaScript/Lv. 0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,11 @@
+function solution(my_string, s, e) {
+  let front = my_string.slice(0, s);
+  let mid = my_string
+    .slice(s, e + 1)
+    .split('')
+    .reverse()
+    .join('');
+  let back = my_string.slice(e + 1);
+
+  return front + mid + back;
+}
