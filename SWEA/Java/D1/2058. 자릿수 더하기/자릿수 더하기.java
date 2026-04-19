f27eddad82d5949632e2e7629ace3ce3607e536d
Seongchan Park <goslow0628@gmail.com>
2026-04-19 16:54:22 +0900

    [D1] Title: 자릿수 더하기, Time: 77 ms, Memory: 24,192 KB -BaekjoonHub

---
+++ SWEA/Java/D1/2058. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        while (N > 0) {
+            sum += N % 10;
+            N /= 10;
+        }
+
+        System.out.println(sum);
+    }
+}
