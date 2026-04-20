b02d3929a2f03422fcc6a7b063e2c144bae54186
Seongchan Park <goslow0628@gmail.com>
2026-04-21 02:33:35 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 76 ms, Memory: 22,400 KB -BaekjoonHub

---
+++ SWEA/Java/D1/1545. 거꾸로 출력해 보아요/README.md
@@ -0,0 +1,15 @@
+# [D1] 거꾸로 출력해 보아요 - 1545 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV2gbY0qAAQBBAS0) 
+
+### 성능 요약
+
+메모리: 22,400 KB, 시간: 76 ms, 코드길이: 454 Bytes
+
+### 제출 일자
+
+2026-04-21 02:33
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Java/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = n; i >= 0; i--) {
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
