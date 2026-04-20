e27d8f8b6ec353db5d4c6f2417bfdfa76ca6566f
Seongchan Park <goslow0628@gmail.com>
2026-04-20 23:50:23 +0900

    [D1] Title: 신문 헤드라인, Time: 70 ms, Memory: 25,600 KB -BaekjoonHub

---
+++ SWEA/Java/D1/2047. 신문 헤드라인/README.md
@@ -0,0 +1,15 @@
+# [D1] 신문 헤드라인 - 2047 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QKsLaAy0DFAUq) 
+
+### 성능 요약
+
+메모리: 25,600 KB, 시간: 70 ms, 코드길이: 315 Bytes
+
+### 제출 일자
+
+2026-04-20 23:50
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Java/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        System.out.println(s.toUpperCase());
+    }
+}
