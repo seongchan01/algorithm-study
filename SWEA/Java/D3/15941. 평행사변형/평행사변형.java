cf4b9ef1e65b6d903cf27c626279a61266fc995f
Seongchan Park <goslow0628@gmail.com>
2026-04-29 16:32:40 +0900

    [D3] Title: 평행사변형, Time: 80 ms, Memory: 24,960 KB -BaekjoonHub

---
+++ SWEA/Java/D3/15941. 평행사변형/평행사변형.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+
+			sb.append("#").append(tc).append(" ").append(N * N).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+}
