05c979ef0fb4a7a16506e718957ea409fa510811
Seongchan Park <goslow0628@gmail.com>
2026-05-03 18:36:10 +0900

    [level 2] Title: 주식가격, Time: 32.55 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/Java/Lv. 2/42584. 주식가격/주식가격.java
@@ -0,0 +1,28 @@
+import java.util.ArrayDeque;
+
+class Solution {
+
+	public static int[] solution(int[] prices) {
+		int n = prices.length;
+		int[] answer = new int[n];
+
+		ArrayDeque<Integer> stack = new ArrayDeque<Integer>();
+		stack.push(0);
+
+		for (int i = 1; i < n; i++) {
+			while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+				int j = stack.pop();
+				answer[j] = i - j;
+			}
+			stack.push(i);
+		}
+
+		while (!stack.isEmpty()) {
+			int j = stack.pop();
+			answer[j] = n - 1 - j;
+		}
+
+		return answer;
+	}
+
+}
