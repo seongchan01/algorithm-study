9128f251532889540d5c8cf1356093b47d61847d
Seongchan Park <goslow0628@gmail.com>
2026-05-04 15:13:41 +0900

    [level 1] Title: 카드 뭉치, Time: 0.10 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/Java/Lv. 1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,32 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+class Solution {
+	public String solution(String[] cards1, String[] cards2, String[] goal) {
+		String answer = "Yes";
+
+		Queue<String> queue1 = new ArrayDeque<String>();
+		Queue<String> queue2 = new ArrayDeque<String>();
+
+		for (int i = 0; i < cards1.length; i++) {
+			queue1.add(cards1[i]);
+		}
+
+		for (int i = 0; i < cards2.length; i++) {
+			queue2.add(cards2[i]);
+		}
+
+		for (int i = 0; i < goal.length; i++) {
+			if (goal[i].equals(queue1.peek())) {
+				queue1.poll();
+			} else if (goal[i].equals(queue2.peek())) {
+				queue2.poll();
+			} else {
+				answer = "No";
+				break;
+			}
+		}
+
+		return answer;
+	}
+}
