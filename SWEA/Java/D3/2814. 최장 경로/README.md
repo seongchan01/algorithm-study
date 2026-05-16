3a7ca27f3cfc570394f1591a272733dbf8ff622c
Seongchan Park <goslow0628@gmail.com>
2026-05-16 15:23:03 +0900

    [D3] Title: 최장 경로, Time: 73 ms, Memory: 25,344 KB -BaekjoonHub

---
+++ SWEA/Java/D3/2814. 최장 경로/README.md
@@ -0,0 +1,15 @@
+# [D3] 최장 경로 - 2814 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV7GOPPaAeMDFAXB) 
+
+### 성능 요약
+
+메모리: 25,344 KB, 시간: 73 ms, 코드길이: 1,328 Bytes
+
+### 제출 일자
+
+2026-05-16 15:22
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Java/D3/2814. 최장 경로/최장 경로.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Solution {
+	static int N, M;
+	static boolean[][] graph;
+	static boolean[] visit;
+	static int answer;
+
+	public static void main(String args[]) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+
+			graph = new boolean[N + 1][N + 1];
+			visit = new boolean[N + 1];
+			answer = 0;
+
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+
+				graph[x][y] = true;
+				graph[y][x] = true;
+			}
+
+			for (int i = 1; i <= N; i++) {
+				dfs(i, 1);
+			}
+
+			sb.append("#").append(tc).append(" ").append(answer).append("\n");
+		}
+
+		System.out.print(sb);
+	}
+
+	static void dfs(int now, int length) {
+		visit[now] = true;
+
+		answer = Math.max(answer, length);
+
+		for (int next = 1; next <= N; next++) {
+			if (graph[now][next] && !visit[next]) {
+				dfs(next, length + 1);
+			}
+		}
+
+		visit[now] = false;
+	}
+
+}
