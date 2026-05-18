cca487ca1cae8874239855e20a799b234265d128
Seongchan Park <goslow0628@gmail.com>
2026-05-19 02:00:16 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 85 ms, Memory: 26,112 KB -BaekjoonHub

---
+++ SWEA/Java/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class Solution {
+
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		for (int tc = 1; tc <= 10; tc++) {
+			int N = Integer.parseInt(br.readLine());
+
+			ArrayList<Integer> list = new ArrayList<>();
+
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				list.add(Integer.parseInt(st.nextToken()));
+			}
+
+			int C = Integer.parseInt(br.readLine());
+
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < C; i++) {
+				String c = st.nextToken();
+
+				if (c.equals("I")) {
+					int x = Integer.parseInt(st.nextToken());
+					int y = Integer.parseInt(st.nextToken());
+
+					for (int j = 0; j < y; j++) {
+						int n = Integer.parseInt(st.nextToken());
+						list.add(x + j, n);
+					}
+				}
+			}
+
+			sb.append("#").append(tc).append(" ");
+
+			for (int i = 0; i < 10; i++) {
+				sb.append(list.get(i)).append(" ");
+			}
+
+			sb.append("\n");
+		}
+
+		System.out.print(sb);
+	}
+}
