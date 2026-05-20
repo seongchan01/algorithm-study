303ce04fa0e426da0320aae0a4007b14ab5b4fde
Seongchan Park <goslow0628@gmail.com>
2026-05-20 14:13:18 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 81 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/Java/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
+++ SWEA/Java/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
@@ -3,71 +3,72 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Solution {
-    public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine());
 
-        for (int tc = 1; tc <= T; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int K = Integer.parseInt(st.nextToken());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int[][] arr = new int[N][N];
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
 
-            for (int i = 0; i < N; i++) {
-                StringTokenizer st1 = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
-                    arr[i][j] = Integer.parseInt(st1.nextToken());
-                }
-            }
+			int[][] arr = new int[N][N];
 
-            int answer = 0;
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
 
-            // 가로
-            for (int i = 0; i < N; i++) {
-                int cnt = 0;
+			int answer = 0;
 
-                for (int j = 0; j < N; j++) {
-                    if (arr[i][j] == 1) {
-                        cnt++;
-                    } else {
-                        if (cnt == K) {
-                            answer++;
-                        }
-                        cnt = 0;
-                    }
-                }
+			// 가로
+			for (int i = 0; i < N; i++) {
+				int cnt = 0;
 
-                if (cnt == K) {
-                    answer++;
-                }
-            }
+				for (int j = 0; j < N; j++) {
+					if (arr[i][j] == 1) {
+						cnt++;
+					} else {
+						if (cnt == M) {
+							answer++;
+						}
+						cnt = 0;
+					}
+				}
 
-            // 세로
-            for (int j = 0; j < N; j++) {
-                int cnt = 0;
+				if (cnt == M) {
+					answer++;
+				}
+			}
 
-                for (int i = 0; i < N; i++) {
-                    if (arr[i][j] == 1) {
-                        cnt++;
-                    } else {
-                        if (cnt == K) {
-                            answer++;
-                        }
-                        cnt = 0;
-                    }
-                }
+			// 세로
+			for (int j = 0; j < N; j++) {
+				int cnt = 0;
 
-                if (cnt == K) {
-                    answer++;
-                }
-            }
+				for (int i = 0; i < N; i++) {
+					if (arr[i][j] == 1) {
+						cnt++;
+					} else {
+						if (cnt == M) {
+							answer++;
+						}
+						cnt = 0;
+					}
+				}
 
-            sb.append("#").append(tc).append(" ").append(answer).append("\n");
-        }
+				if (cnt == M) {
+					answer++;
+				}
+			}
 
-        System.out.println(sb);
-    }
+			sb.append("#").append(tc).append(" ").append(answer).append("\n");
+		}
+
+		System.out.print(sb);
+	}
 }
