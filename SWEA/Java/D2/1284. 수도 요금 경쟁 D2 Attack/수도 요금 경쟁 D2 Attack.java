409859ca0c7e988c9905a2cf7735d7d3903e5f39
Seongchan Park <goslow0628@gmail.com>
2026-05-20 13:59:04 +0900

    [D2] Title: 수도 요금 경쟁 D2 Attack, Time: 67 ms, Memory: 22,912 KB -BaekjoonHub

--- SWEA/Java/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.java
+++ SWEA/Java/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.java
@@ -3,31 +3,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Solution {
-    public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine());
 
-        for (int tc = 1; tc <= T; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int P = Integer.parseInt(st.nextToken());
-            int Q = Integer.parseInt(st.nextToken());
-            int R = Integer.parseInt(st.nextToken());
-            int S = Integer.parseInt(st.nextToken());
-            int W = Integer.parseInt(st.nextToken());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int chargeA = P * W;
-            int chargeB;
-            if (W <= R) {
-                chargeB = Q;
-            } else {
-                chargeB = Q + ((W - R) * S);
-            }
+			int P = Integer.parseInt(st.nextToken());
+			int Q = Integer.parseInt(st.nextToken());
+			int R = Integer.parseInt(st.nextToken());
+			int S = Integer.parseInt(st.nextToken());
+			int W = Integer.parseInt(st.nextToken());
 
-            sb.append("#").append(tc).append(" ").append(chargeA < chargeB ? chargeA : chargeB).append("\n");
-        }
+			int A = P * W;
+			int B;
 
-        System.out.print(sb);
-    }
+			if (W <= R) {
+				B = Q;
+			} else {
+				B = Q + ((W - R) * S);
+			}
+
+			int answer = Math.min(A, B);
+
+			sb.append("#").append(tc).append(" ").append(answer).append("\n");
+
+		}
+		System.out.print(sb);
+	}
+
 }
