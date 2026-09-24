c6b06c7e803d3f70d23f2507a9ef2e2357ae59df
seongchan <goslow0628@gmail.com>
2026-09-24 22:37:52 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 86 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/Java/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/Java/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -1,30 +1,39 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 
-public class Solution { // 1226. 미로1
-	static int N = 16;
+/**
+ * [문제 분석]
+ * 16*16 행렬의 미로에서 시작점에서 출발해서 도착점에 도착할 수 있는지 판별
+ * 시작점: (2,2)
+ * 도착점: (3,3)
+ * 
+ * [풀이]
+ * 출발점에서 bfs
+ */
+
+public class Solution { // D4. 미로1
+
 	static int[][] arr;
-	static boolean[][] visit;
-	static int[] dx = { 0, -1, 0, 1 };
-	static int[] dy = { 1, 0, -1, 0 };
-	static int result;
+	static int[] dx = { 0, 0, 1, -1 };
+	static int[] dy = { 1, -1, 0, 0 };
 
 	public static void main(String[] args) throws Exception {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
-		for (int t = 1; t <= 10; t++) {
-			int tc = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= 10; tc++) {
+			int T = Integer.parseInt(br.readLine());
 
-			arr = new int[N][N];
-			visit = new boolean[N][N];
-
+			arr = new int[16][16];
 			int startX = 0;
 			int startY = 0;
 
-			for (int i = 0; i < N; i++) {
+			for (int i = 0; i < 16; i++) {
 				String line = br.readLine();
-				for (int j = 0; j < N; j++) {
+
+				for (int j = 0; j < 16; j++) {
 					arr[i][j] = line.charAt(j) - '0';
 
 					if (arr[i][j] == 2) {
@@ -34,37 +43,50 @@ public class Solution { // 1226. 미로1
 				}
 			}
 
-			result = 0;
+			int answer = bfs(startX, startY);
 
-			dfs(startX, startY);
-
-			sb.append("#").append(tc).append(" ").append(result).append("\n");
+			sb.append("#").append(T).append(" ").append(answer).append("\n");
 		}
 
 		System.out.println(sb);
 	}
 
-	static void dfs(int x, int y) {
-		if (result == 1)
-			return;
+	static int bfs(int startX, int startY) {
+		boolean[][] visit = new boolean[16][16];
 
-		visit[x][y] = true;
+		ArrayDeque<int[]> queue = new ArrayDeque<int[]>();
+		queue.offer(new int[] { startX, startY });
+		visit[startX][startY] = true;
 
-		for (int next = 0; next < 4; next++) {
-			int nx = x + dx[next];
-			int ny = y + dy[next];
+		while (!queue.isEmpty()) {
+			int[] now = queue.poll();
+			int x = now[0];
+			int y = now[1];
 
-			if (nx < 0 || nx >= N || ny < 0 || ny >= N || visit[nx][ny] || arr[nx][ny] == 1) {
-				continue;
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+
+				if (nx < 0 || nx >= 16 || ny < 0 || ny >= 16) {
+					continue;
+				}
+
+				if (arr[nx][ny] == 3) {
+					return 1;
+				}
+
+				if (arr[nx][ny] == 1 || visit[nx][ny]) {
+					continue;
+				}
+
+				visit[nx][ny] = true;
+
+				queue.offer(new int[] { nx, ny });
+
 			}
-
-			if (arr[nx][ny] == 3) {
-				result = 1;
-				return;
-			}
-
-			dfs(nx, ny);
 		}
+
+		return 0;
 	}
 
 }
