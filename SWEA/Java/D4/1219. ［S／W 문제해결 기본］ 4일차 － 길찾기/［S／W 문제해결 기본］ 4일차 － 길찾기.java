7595d12b8db8702bb38732b3df291b24a45a4961
seongchan <goslow0628@gmail.com>
2026-09-26 18:25:45 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 길찾기, Time: 88 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/Java/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/［S／W 문제해결 기본］ 4일차 － 길찾기.java
+++ SWEA/Java/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/［S／W 문제해결 기본］ 4일차 － 길찾기.java
@@ -1,76 +1,88 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Solution { // 1219. 길찾기
+/**
+ * [문제 해결]
+ * A도시에서 출발해서 B도시로 가는길이 존재하는지 조사.
+ * 길의 중간 중간에는 최대 2개의 갈림길 존재 + 일방 통행만 가능
+ * 
+ * A와 B는 0과 99고정
+ * 모든 길은 순서쌍: 2번에서 출발할 수 있는 길은 (2,5), (2,9)
+ * 가는 길의 상관없이 한가지 길이라도 존재하면 길이 존재
+ * 화살표 거슬러 갈 수 없음
+ * 
+ * [풀이]
+ * 방향 그래프로 길을 저장한다
+ * 0번 도시부터 BFS로 탐색하면서 99번 도시에 도착할 수 있는지 확인한다
+ * 99번에 도착하면 1, 도착하지 못하면 0을 출력한다.
+ */
 
-	static int[] arr1;
-	static int[] arr2;
-	static int result;
-	static boolean[] visit;
+public class Solution { // D4. 길찾기
+
+	static ArrayList<Integer>[] graph;
 
 	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
-		for (int T = 1; T <= 10; T++) {
+		for (int tc = 1; tc <= 10; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-
-			int tc = Integer.parseInt(st.nextToken());
+			int T = Integer.parseInt(st.nextToken());
 			int N = Integer.parseInt(st.nextToken());
 
-			arr1 = new int[100];
-			Arrays.fill(arr1, -1);
-			arr2 = new int[100];
-			Arrays.fill(arr2, -1);
+			graph = new ArrayList[100];
+
+			for (int i = 0; i < 100; i++) {
+				graph[i] = new ArrayList<>();
+			}
 
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
 
-				if (arr1[a] == -1) {
-					arr1[a] = b;
-				} else {
-					arr2[a] = b;
-				}
+				graph[from].add(to);
 			}
 
-			visit = new boolean[100];
-			result = 0;
-
-			dfs(0);
+			int result = bfs();
 
 			sb.append("#").append(tc).append(" ").append(result).append("\n");
 		}
 
 		System.out.println(sb);
-
 	}
 
-	static void dfs(int now) {
+	static int bfs() {
+		boolean[] visit = new boolean[100];
 
-		if (now == 99) {
-			result = 1;
-			return;
+		Queue<Integer> queue = new ArrayDeque<>();
+		queue.offer(0);
+		visit[0] = true;
+
+		while (!queue.isEmpty()) {
+
+			int now = queue.poll();
+
+			if (now == 99) {
+				return 1;
+			}
+
+			for (int next : graph[now]) {
+				if (!visit[next]) {
+
+					visit[next] = true;
+					queue.offer(next);
+				}
+			}
 		}
 
-		visit[now] = true;
-
-		int next1 = arr1[now];
-		if (next1 != -1 && !visit[next1]) {
-			dfs(next1);
-		}
-
-		if (result == 1) {
-			return;
-		}
-
-		int next2 = arr2[now];
-		if (next2 != -1 && !visit[next2]) {
-			dfs(next2);
-		}
+		return 0;
 	}
 
 }
